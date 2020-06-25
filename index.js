4881cb5625c8d544882abc8ead0aca3d33982d93
reficul109 <38768149+reficul109@users.noreply.github.com>
2020-06-25 11:22:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const http = require('http')
+const express = require('express')
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(${{secrets.PORT}}, () => console.log('Listening!'));
+
 
 //Ready
 client.on('ready', () => {
