2ed254edfa980fce85444720444ef915889d291f
reficul109 <leogamer125@hotmail.com>
2020-07-29 16:58:07 +0000

    eep

--- commands/imp.js
+++ commands/imp.js
@@ -6,4 +6,4 @@ module.exports = {
   thumbs: true,
 	execute(Discord, client, message, caller, origin, msgAtt, embed, embed2, db, a, f, v) {
     if (message.argresult.length > 500) {return origin.send('(Do you really need so many letters for this?)')}
-    client.channels.cache.get("565656454170804254").send('``' + caller.id + '`` - ' + caller.tag + ' (**' + caller.DB.keys + '** keys) sent: ' + message.argresult + '\n\n(Raw Message: ``' + message.argresult  + '``)', {files: msgAtt})}}
+    client.channels.cache.get(v.impChann).send('``' + caller.id + '`` - ' + caller.tag + ' (**' + caller.DB.keys + '** keys) sent: ' + message.argresult + '\n\n(Raw Message: ``' + message.argresult  + '``)', {files: msgAtt})}}
