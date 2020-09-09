afb1c4ea44f47dadf8945b1202c81f6b3dca10eb
reficul109 <leogamer125@hotmail.com>
2020-09-09 23:01:49 +0000

    final

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.once('ready', () => {
 //Avys
 client.on('userUpdate', (oldUser, newUser) => {
   newUser.DB = client.modDB.get(newUser.id)
-  if (newUser.DB && newUser.DB.mimicRole === 'Y') {
+  if (oldUser.avatarURL() !== newUser.avatarURL() && newUser.DB && newUser.DB.mimicRole === 'Y') {
     getColors(newUser.displayAvatarURL({format: 'png', dynamic: true})).then(colors => {
     client.channels.cache.get("426520047301951509").send('<@' + newUser.id + '>, choose a color! [Reply "1", "2", etc...]\nhttps://encycolorpedia.com/' + colors[0].toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[1].toString().substring(1) + '\nhttps://encycolorpedia.com/' + colors[2].toString().substring(1))
     const collector = new Discord.MessageCollector(client.channels.cache.get("426520047301951509"), m => m.author.id === newUser.id, {time: 600000})
